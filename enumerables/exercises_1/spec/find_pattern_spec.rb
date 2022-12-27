448706cf28e646d1fcd77bc243bfb62fd1c270bd
Caleb Thomas <cjthomas00@comcast.net>
2022-12-27 11:50:52 -0700

    complete find_pattern_spec file through exercise 7.

--- enumerables/exercises_1/spec/find_pattern_spec.rb
+++ enumerables/exercises_1/spec/find_pattern_spec.rb
@@ -12,49 +12,66 @@ RSpec.describe 'find pattern' do
     expect(found).to eq("unicorn")
   end
 
-  xit 'no waldo' do
+  it 'no waldo' do
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
     found = nil
     words.each do |word|
-      # Your code goes here
+      found << word if word.include?("waldo")
     end
     expect(found).to eq(nil)
   end
 
-  xit 'found waldo' do
+  it 'found waldo' do
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
     found = nil
-    # Your code goes here
+    words.each do |word|
+      found = word if word.include?("waldo")
+    end
     expect(found).to eq("waldo")
   end
 
-  xit 'no three letter words' do
+  it 'no three letter words' do
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+    found = nil
+    words.each do |word|
+      found = word if word.length == 3
+    end
     expect(found).to eq(nil)
   end
 
-  xit 'finds 13' do
+  it 'finds 13' do
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = nil
+    numbers.each do |num|
+      found = num if num == 13
+    end
     expect(found).to eq(13)
   end
 
-  xit 'first even number' do
+  it 'first even number' do
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = nil
+    numbers.each do |num|
+      found = num if num = 10
+    end
     expect(found).to eq(10)
   end
 
   xit 'first multiple of 3' do
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+    found = []
+    numbers.each do |num|
+      found << num if num % 3 == 0
+    end
     expect(found).to eq(9)
   end
 
   xit 'first word starting with q' do
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
+    found = nil
+    words.each do |word|
+      found = word if word.start_with?("q")
+    end
     expect(found).to eq("quill")
   end
 
@@ -66,7 +83,11 @@ RSpec.describe 'find pattern' do
 
   xit 'first number greater than 20' do
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    found = []
+    numbers.each do |num|
+      found << num if num > 20
+    end
+     found.first
     expect(found).to eq(21)
   end
 end
